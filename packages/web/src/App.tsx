060b68e11091e3e614fcdc2dc80f20b361ade63d
Michael Ronald <michaelronald.dr@gmail.com>
2024-10-09 16:59:46 +0530

    add sign-in and sign-out flow to react app

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route, Navigate } from "react-router-dom";
+import { AuthenticateWithRedirectCallback, useAuth } from "@clerk/clerk-react";
+import Login from "./Login";
+import { Button, Container, Title } from "@mantine/core";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { isSignedIn, signOut } = useAuth();
+
+  console.log("isSignedIn", isSignedIn);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route
+        path="sso-callback"
+        element={<AuthenticateWithRedirectCallback />}
+      />
+      <Route
+        path="/"
+        element={
+          isSignedIn ? (
+            <Container
+              size="xs"
+              style={{ textAlign: "center", marginTop: "50px" }}
+            >
+              <Title order={2} mb="xl">
+                Sign out by clicking the button below
+              </Title>
+              <Button
+                size="lg"
+                color="red"
+                onClick={() => {
+                  signOut();
+                }}
+              >
+                Sign out
+              </Button>
+            </Container>
+          ) : (
+            <Navigate to="/login" replace />
+          )
+        }
+      />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
